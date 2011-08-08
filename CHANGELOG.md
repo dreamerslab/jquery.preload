34b70158e9d49cc933cf410a58141fb40b576863
ben <ben@dreamerslab.com>
2011-08-08 23:57:56 +0800

    Fix `$` namespace conflict

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # jQuery Preload Plugin CHANGELOG
 
+## 1.0.1
+
+* Fix `$` namespace conflict
+
+
+
 ## 1.0.0
 
 * First stable release
--- jquery.preload.js
+++ jquery.preload.js
@@ -1,13 +1,14 @@
 /*! Copyright 2011, Ben Lin (http://dreamerslab.com/)
 * Licensed under the MIT License (LICENSE.txt).
 *
-* Version: 1.0.0
+* Version: 1.0.1
 *
 * Requires: jQuery 1.2.3+
 */
-
-$.preload = function(){
-  var tmp = [], i = arguments.length;
-  // reverse loop run faster
-  for( ; i-- ; ) tmp.push( $( '<img />' ).attr( 'src', arguments[ i ] ));
-};
+;( function( $ ){
+  $.preload = function(){
+    var tmp = [], i = arguments.length;
+    // reverse loop run faster
+    for( ; i-- ; ) tmp.push( $( '<img />' ).attr( 'src', arguments[ i ]));
+  };
+})( jQuery );
