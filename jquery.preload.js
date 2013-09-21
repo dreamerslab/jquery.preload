1fbbcb1c05c64a852f034cd7b8b43921656c9f7f
mitzip <github@mitzip.com>
2013-09-21 17:07:20 -0500

    Added array based paths
    Allowed arrays of image paths to be past as a single argument, instead of each image path needing to be it's own string argument. NOTE: Resulted in requiring jQuery 1.3+ and bumped semantic version.

--- jquery.preload.js
+++ jquery.preload.js
@@ -1,14 +1,15 @@
 /*! Copyright 2011, Ben Lin (http://dreamerslab.com/)
 * Licensed under the MIT License (LICENSE.txt).
 *
-* Version: 1.0.2
+* Version: 1.1.0
 *
-* Requires: jQuery 1.2.3+
+* Requires: jQuery 1.3+
 */
 ;( function( $ ){
   $.preload = function(){
-    var tmp = [], i = arguments.length;
+    var imgs = $.isArray(arguments[ 0 ]) ? arguments[ 0 ] : arguments;
+    var tmp = [], i = imgs.length;
     // reverse loop run faster
-    for( ; i-- ; ) tmp.push( $( '<img />' ).attr( 'src', arguments[ i ]));
+    for( ; i-- ; ) tmp.push( $( '<img />' ).attr( 'src', imgs[ i ]));
   };
 })( jQuery );
